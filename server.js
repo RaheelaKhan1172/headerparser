110be431b8bd96cc9250828ca686acd6c1896642
Raheela Khan <raheelak20@gmail.com>
2016-01-09 18:11:51 -0800

    done?

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+
+
+app.get('/',function(req,res) {
+
+  var i =  req.connection.remoteAddress;
+  var op = req.headers['user-agent'].split(/\(/)[1];
+  console.log('yay',op);
+  var d = {
+    "IP Address": i,
+    "Language": req.headers['accept-language'],
+    "Operating System": op 
+  }
+
+  console.log(req.headers['user-agent'],req.headers['accept-language']);
+  res.send(d);
+});
+
+app.listen(3030);
